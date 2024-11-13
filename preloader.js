34dbf8db247a47b4a03b22f3b2b48caf1b8853a9
azulejosbrihuega <167983944+azulejosbrihuega@users.noreply.github.com>
2024-11-13 17:44:47 +0100

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,13 +1,22 @@
 <script>
-    // Oculta el preloader después de que toda la página haya cargado
-    window.addEventListener("load", function() {
+    document.addEventListener("DOMContentLoaded", function() {
         const preloader = document.getElementById("preloader");
-        preloader.style.transition = "opacity 0.5s ease"; // Transición de desvanecimiento
-        preloader.style.opacity = "0"; // Desvanece el preloader
 
-        // Espera a que termine la transición antes de eliminar el preloader
-        setTimeout(() => {
-            preloader.style.display = "none";
-        }, 500); // Tiempo de espera coincidente con la transición
+        // Oculta el preloader ya sea después de que la página haya cargado o tras un máximo de 3 segundos
+        function hidePreloader() {
+            preloader.style.transition = "opacity 0.5s ease"; // Transición de desvanecimiento
+            preloader.style.opacity = "0"; // Desvanece el preloader
+
+            // Espera a que termine la transición antes de eliminar el preloader
+            setTimeout(() => {
+                preloader.style.display = "none";
+            }, 500); // Coincide con el tiempo de transición de 0.5s
+        }
+
+        // Configura el temporizador de 3 segundos
+        setTimeout(hidePreloader, 3000);
+
+        // También oculta el preloader cuando la página se carga por completo
+        window.addEventListener("load", hidePreloader);
     });
 </script>
