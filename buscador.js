c3ab5aa29cdb110d699b605b5342950c562e878a
azulejosbrihuega <167983944+azulejosbrihuega@users.noreply.github.com>
2025-01-23 19:19:51 +0100

    Actualización Algoritmo buscador

--- buscador.js
+++ buscador.js
@@ -3,23 +3,40 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById('searchInput').addEventListener('input', function() {
         var searchValue = this.value.toLowerCase().trim();
-        
+
         if (searchValue === '') {
             keywordsList.style.display = 'none'; 
             return;
         }
 
-        var lis = keywordsList.getElementsByTagName('li');
+        var lis = Array.from(keywordsList.getElementsByTagName('li'));
+
         
-        Array.from(lis).forEach(function(li) {
+        var matches = [];
+
+        lis.forEach(function(li) {
             var keyword = li.textContent.toLowerCase();
-            if (keyword.startsWith(searchValue)) {
-                li.style.display = 'block'; 
+            var index = keyword.indexOf(searchValue);
+
+            if (index !== -1) {
+                
+                matches.push({
+                    element: li,
+                    relevance: index
+                });
             } else {
                 li.style.display = 'none'; 
             }
         });
 
-        keywordsList.style.display = 'block'; 
+        matches.sort(function(a, b) {
+            return a.relevance - b.relevance;
+        });
+
+        matches.forEach(function(match) {
+            match.element.style.display = 'block';
+        });
+
+        keywordsList.style.display = matches.length > 0 ? 'block' : 'none';
     });
 });
